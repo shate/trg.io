b43592fb9dbb45f0ac80558d7b45eb0ea622c335
shate <shate@ya.ru>
2019-11-18 14:36:40 +0300

    init

---
+++ public/precache-manifest.2a6b8abe83d4c1a0229e92a409122d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f69f07672869608cd688e9e9069eca6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5840851afdaa532e6521",
+    "url": "./static/css/2.6399008a.chunk.css"
+  },
+  {
+    "revision": "32aa88c588f100b34552",
+    "url": "./static/css/main.ad260ae6.chunk.css"
+  },
+  {
+    "revision": "5840851afdaa532e6521",
+    "url": "./static/js/2.e828cc6e.chunk.js"
+  },
+  {
+    "revision": "32aa88c588f100b34552",
+    "url": "./static/js/main.1cc452ac.chunk.js"
+  },
+  {
+    "revision": "b924f7d6bd20900a94b1",
+    "url": "./static/js/runtime~main.6b272885.js"
+  }
+]);
